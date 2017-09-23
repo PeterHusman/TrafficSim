c25b19dd412bc599e1fcbf50246b8969fbe8b87a
Peter H <mlhusman@earthlink.net>
2017-09-23 16:34:18 -0700

    Worked on car physics

---
+++ TrafficSim/Car.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrafficSim
+{
+    public class Car
+    {
+        public Vector2 Position { get; set; }
+
+        float speed;
+        public float Speed { get { return speed; } set { speed = Math.Min(value, MaxSpeed); } }
+
+        public float MaxSpeed { get; set; }
+
+        public float TargetSpeed { get; set; }
+
+        public Direction Direction { get; set; }
+
+        public float Acceleration { get; set; }
+
+        public float Deceleration { get; set; }
+
+        public Car(Vector2 position, float acceleration, float deceleration, Direction dir, float maxSpeed)
+        {
+            Position = position;
+            Acceleration = acceleration;
+            Deceleration = deceleration;
+            Direction = dir;
+            MaxSpeed = maxSpeed;
+        }
+
+
+        public void Update()
+        {
+            if(TargetSpeed > speed)
+            {
+                Speed += Acceleration;
+            }
+            else if(TargetSpeed < speed)
+            {
+                Speed -= Deceleration;
+            }
+            if((TargetSpeed - speed)*(TargetSpeed-speed) < 4)
+            {
+                Speed = TargetSpeed;
+            }
+
+
+            Position = new Vector2(Position.X + (Direction == Direction.East ? speed : (Direction == Direction.West ? -speed : 0)), Position.Y + (Direction == Direction.North ? -speed : (Direction == Direction.South ? speed : 0)));
+
+
+
+        }
+
+
+    }
+}
---
+++ TrafficSim/Game1.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace TrafficSim
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+
+        MouseState mouse;
+        MouseState oldMouse;
+
+        KeyboardState keyboard;
+        KeyboardState oldKeyboard;
+
+        List<Car> cars = new List<Car>();
+
+        public static Texture2D Pixel;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            IsMouseVisible = true;
+
+            graphics.PreferredBackBufferHeight = 1080;
+            graphics.PreferredBackBufferWidth = 1920;
+            graphics.ApplyChanges();
+
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            Pixel = new Texture2D(GraphicsDevice, 1, 1);
+            Pixel.SetData(new Color[] { Color.White });
+
+            cars.Add(new Car(Vector2.One*1000,1f,0.3f,Direction.North, 100f));
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            mouse = Mouse.GetState();
+            keyboard = Keyboard.GetState();
+
+            if(keyboard.IsKeyDown(Keys.W))
+            {
+                cars[0].Direction = Direction.North;
+            }
+            else if (keyboard.IsKeyDown(Keys.D))
+            {
+                cars[0].Direction = Direction.East;
+            }
+            else if (keyboard.IsKeyDown(Keys.S))
+            {
+                cars[0].Direction = Direction.South;
+            }
+            else if (keyboard.IsKeyDown(Keys.A))
+            {
+                cars[0].Direction = Direction.West;
+            }
+            else if(keyboard.IsKeyDown(Keys.Up) && oldKeyboard.IsKeyUp(Keys.Up))
+            {
+                cars[0].TargetSpeed+= 10;
+            }
+            else if (keyboard.IsKeyDown(Keys.Down) && oldKeyboard.IsKeyUp(Keys.Down))
+            {
+                cars[0].TargetSpeed-= 10;
+            }
+            if(mouse.LeftButton == ButtonState.Pressed && oldMouse.LeftButton == ButtonState.Released)
+            {
+                cars.Add(new Car(Vector2.One * 1000, 0.1f, 2f, Direction.North, 100f));
+            }
+
+            foreach (Car car in cars)
+            {
+                car.Update();
+            }
+
+
+            oldKeyboard = keyboard;
+            oldMouse = mouse;
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            float scalar = 0.1f;
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+
+            foreach(Car car in cars)
+            {
+                spriteBatch.Draw(Pixel, new Rectangle((int)(car.Position.X*scalar), (int)(car.Position.Y*scalar), (int)((((int)car.Direction % 2 + 1)) * 50*scalar), (int)(((((int)car.Direction + 1) % 2 + 1)) * 50*scalar)), Color.Red);
+            }
+
+            
+
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
