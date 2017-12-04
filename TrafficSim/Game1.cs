030ac904250fda1d0046dba18aaadecdf7066736
Peter H <mlhusman@earthlink.net>
2017-12-03 16:32:21 -0800

    Collected data and cited sources. TODO: Make data analyzer able to retrieve a specific trial.

--- TrafficSim/Game1.cs
+++ TrafficSim/Game1.cs
@@ -217,6 +217,9 @@ namespace TrafficSim
                     bool carLast = street.Cars.Find(car).Previous == null;
 
                     bool carInSlowDownRange = false;
+
+                    //List<float> carSpeeds = new List<float>();
+
                     //Brake if cars are ahead
                     if (!carFirst)
                     {
@@ -256,7 +259,7 @@ namespace TrafficSim
                     {
                         float distance = car.Direction == Direction.West ? (car.Position.X - inter.Position.X) : (car.Direction == Direction.East ? (inter.Position.X - car.Position.X) : (car.Direction == Direction.South ? (inter.Position.Y-car.Position.Y) : (car.Direction == Direction.North ? (car.Position.Y-inter.Position.Y) : 0)));
 
-                        //TODO: Fix cars intersecting with all intersections in one axis
+                        
                         if(distance < car.IntersectionBrakingDistance && (int)car.Direction % 2 != (int)inter.Direction && distance > 0 && Math.Abs(car.Position.X - inter.Position.X) <= car.IntersectionBrakingDistance && Math.Abs(car.Position.Y - inter.Position.Y) <= car.IntersectionBrakingDistance)
                         {
                             car.TargetSpeed = Math.Min(Math.Max(0f, car.TargetSpeed * (distance - 100) / car.IntersectionBrakingDistance),car.TargetSpeed);
